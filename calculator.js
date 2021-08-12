52e0c7b36b7fabe5cfbe85613c7997a0cefdc6cc
Artur <79041911+Fateyv@users.noreply.github.com>
2021-08-12 13:28:38 +0300

    added all

---
+++ calculator.js
@@ -0,0 +1,11 @@
+// const add = require('./add');
+// const sub = require('./sub');
+// const mult = require('./mult');
+// const div = require('./div');
+
+module.exports = {
+    add:require('./add'),
+    sub: require('./sub'),
+    mult: require('./mult'),
+    div: require('./div'),
+}
